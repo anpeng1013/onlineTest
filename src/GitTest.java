1a45a1a8843448baefe39876bf84b6f03c7f9da8
anpeng1013 <anpeng1013@163.com>
2024-06-08 17:38:15 +0800

    master commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello anpeng");
         System.out.println("master update");
         System.out.println("hot-fix");
+        System.out.println("hello anpeng");
     }
 }
