75e55acbc16f874f5a7a0a372221cf7991c6386c
anpeng1013 <anpeng1013@163.com>
2024-06-08 16:55:00 +0800

    hot-fix first commit

---
+++ src/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName: GitTest.java
+ * @Description:
+ * @Author: anpeng
+ * @Date: 2024/6/8 16:53
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello anpeng");
+    }
+}
