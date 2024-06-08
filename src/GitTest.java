5dd7511510e53b03c49d3b0558846b06c0c74c62
anpeng1013 <anpeng1013@163.com>
2024-06-08 17:50:03 +0800

    test clone

--- src/GitTest.java
+++ src/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hot-fix push");
         System.out.println("hot-fix pull test");
         System.out.println("hello anpeng");
+        System.out.println("test clone");
     }
 }
