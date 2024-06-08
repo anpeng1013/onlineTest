c3abe3d9e7cad97f724a5f20c1cdcd0a05ef86de
anpeng1013 <107192251+anpeng1013@users.noreply.github.com>
2024-06-08 17:31:37 +0800

    hot-fix pull test

--- src/GitTest.java
+++ src/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("master update");
         System.out.println("hot-fix");
         System.out.println("hot-fix push");
+        System.out.println("hot-fix pull test");
     }
 }
